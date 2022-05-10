67e4d5d6a3f0cb412bc7af7649d4f773a5c6a5a0
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-05-10 15:50:58 -0300

    feat: added backend integration using axios

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,5 +1,5 @@
 import { ChatTeardropDots } from 'phosphor-react-native';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
@@ -14,11 +14,22 @@ import { Success } from '../Success'
 export type FeedbackType = keyof typeof feedbackTypes;
 
 function Widget() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
 
   const bottomSheetRef = useRef<BottomSheet>(null);
 
   function handleOpen() {
     bottomSheetRef.current?.expand();
+  }
+
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+    setFeedbackSent(false);
+  }
+
+  function handleFeedbackSent(){
+    setFeedbackSent(true);
   }
 
   return (
@@ -40,7 +51,23 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        <Success />
+        {
+          feedbackSent ?
+            <Success onSendAnotherFeedback={handleRestartFeedback} />
+            :
+            <>
+              {
+                feedbackType ? 
+                  <Form 
+                    feedbackType={feedbackType}
+                    onFeedbackCanceled={handleRestartFeedback}
+                    onFeedbackSent={handleFeedbackSent}
+                  />
+                  :
+                  <Options onFeedbackTypeChanged={setFeedbackType} />
+              }
+            </>
+        }
       </BottomSheet>
     </>
   );
